965536773ecf43d4d8b6943138b37115b09faa53
Aditya Andar Rahim <adityandar@gmail.com>
2024-08-24 13:20:42 +0700

    3rd video

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"os"
+	"restaurant/routes"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func main() {
 	port := os.Getenv("PORT")
@@ -14,4 +18,17 @@ func main() {
 	}
 
 	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
 }
